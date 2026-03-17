bb30ed24e47680a64d7fe8da81bdd1cb3316806a
Gabi Vicu <gabi.vicu@gmail.com>
2026-03-17 15:33:07 +0200

    New entities and migretions for db relationshipas

--- src/Entity/MicroPost.php
+++ src/Entity/MicroPost.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Repository\MicroPostRepository;
 use App\Validator\MicroPostValidator;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Mapping\ClassMetadata;
 
@@ -31,6 +33,17 @@ class MicroPost
 
     #[ORM\Column(options: ['default' => 0])]
     private int $likes = 0;
+
+    /**
+     * @var Collection<int, Comment>
+     */
+    #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'post', orphanRemoval: true)]
+    private Collection $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -81,6 +94,36 @@ class MicroPost
     public function setLikes(int $likes): static
     {
         $this->likes = $likes;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): static
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setPost($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): static
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getPost() === $this) {
+                $comment->setPost(null);
+            }
+        }
 
         return $this;
     }
